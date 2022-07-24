b6d898a6fd4b4d6b7c50672554238c5d2b46e1e2
Kartik Dholakia <kartikdholakia5555@gmail.com>
2022-07-24 12:40:45 +0530

    Updates

---
+++ precache-manifest.a26261f6ad92f91e27975372a86e7e48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7def6f3c89c7cb88f0ac61e8a53c8fb3",
+    "url": "/decentralized-storage/index.html"
+  },
+  {
+    "revision": "a3922f846a86642fb1e3",
+    "url": "/decentralized-storage/static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "8d7a9e85f78c4c835d69",
+    "url": "/decentralized-storage/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "a3922f846a86642fb1e3",
+    "url": "/decentralized-storage/static/js/2.c28a7d3b.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "/decentralized-storage/static/js/2.c28a7d3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d7a9e85f78c4c835d69",
+    "url": "/decentralized-storage/static/js/main.f5409ff8.chunk.js"
+  },
+  {
+    "revision": "058125d01008ecb76296",
+    "url": "/decentralized-storage/static/js/runtime-main.cabf885f.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "/decentralized-storage/static/media/box.df8b84d5.png"
+  }
+]);
